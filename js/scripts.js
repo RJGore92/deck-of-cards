848f26f7a5f7f6a36f1e418b7712d5e308cca32c
Robert Gore <DarkburstGX@gmail.com>
2018-12-21 12:44:09 -0800

    second commit of December 21, 2018.  Separates more logic from core program into established functions.

--- js/scripts.js
+++ js/scripts.js
@@ -31,12 +31,8 @@ function dealCard(dealDeck){
   }
 }
 
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-$(document).ready(function() {
+function initialDisplayAndDeck(suits, cards)
+{
   var suits = ["clubs", "diamonds", "spades", "hearts"]
   var cards = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
   suits.forEach(function(suit) {
@@ -50,7 +46,9 @@ $(document).ready(function() {
        preShuffledDeck.push(`${card} of ${suit}`);
     });
   });
-  // preShuffledDeck = deck;
+}
+
+function prepInitialShuffle() {
   for (var i = 52; i > 0; i--) {
     var targetSlot = getRandomInt(i);
     var targetCard = preShuffledDeck[targetSlot];
@@ -83,7 +81,62 @@ $(document).ready(function() {
     preShuffledDeck.splice(targetSlot, 1);
 
   }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+$(document).ready(function() {
+  var suits = ["clubs", "diamonds", "spades", "hearts"]
+  var cards = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+  initialDisplayAndDeck(suits,cards);
+  // suits.forEach(function(suit) {
+  //   $("div#output-row").append(
+  //     `<div class='col-md-3'><h5 class='${suit}'>${suit}</h5><ul id='${suit}'></ul></div>`
+  //   );
+  //   cards.forEach(function(card){
+  //      $(`ul#${suit}`).append(
+  //        `<li class='${suit}'>${card} of ${suit}</li>`
+  //      );
+  //      preShuffledDeck.push(`${card} of ${suit}`);
+  //   });
+  // });
+  // preShuffledDeck = deck;
+  // for (var i = 52; i > 0; i--) {
+  //   var targetSlot = getRandomInt(i);
+  //   var targetCard = preShuffledDeck[targetSlot];
+  //   var splitCard = targetCard.split(" ");
+  //   console.log(splitCard);
+  //   splitSuit = splitCard[2];
+  //   console.log(splitSuit);
+  //   if (i > 39) {
+  //     $("ul#deck-output-a").append(
+  //       "<li class='"+ splitSuit + "'>" + preShuffledDeck[targetSlot] + "</li>"
+  //     );
+  //   }
+  //   else if (i > 26) {
+  //     $("ul#deck-output-b").append(
+  //       "<li class='"+ splitSuit + "'>" + preShuffledDeck[targetSlot] + "</li>"
+  //     );
+  //   }
+  //   else if (i > 13) {
+  //     $("ul#deck-output-c").append(
+  //       "<li class='"+ splitSuit + "'>" + preShuffledDeck[targetSlot] + "</li>"
+  //     );
+  //   }
+  //   else {
+  //     $("ul#deck-output-d").append(
+  //       "<li class='"+ splitSuit + "'>" + preShuffledDeck[targetSlot] + "</li>"
+  //     );
+  //   }
+  //   dealingDeck.push(preShuffledDeck[targetSlot]);
+  //
+  //   preShuffledDeck.splice(targetSlot, 1);
+  //
+  // }
   // dealingDeck = postShuffledDeck;
+  prepInitialShuffle();
   $("button#dealing-button").click(function(){
     dealCard(dealingDeck);
   });
